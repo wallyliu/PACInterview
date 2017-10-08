eb2b6ebc5450be193ba43f5eb3c92275fdcbcb16
wallyliu <briskviva@gmail.com>
2017-10-08 16:56:29 +0800

    add txt parser

---
+++ 4_txt_parser.py
@@ -0,0 +1,17 @@
+# -*- utf-8 -*-
+import re, sys, pprint
+
+def parser(tpath):
+    f = open(tpath, 'r')
+    raw_data = f.read().split('\n')
+
+    result = []
+    for index, value in enumerate(raw_data):
+        if re.findall(r'[0-9]{4} -', value) != []:
+            result.append(raw_data[index-1].replace('[HTML] ', ''))
+    return result
+
+if __name__ == '__main__':
+    tpath = sys.argv[1]
+    result = parser(tpath)
+    pprint.pprint(result)
